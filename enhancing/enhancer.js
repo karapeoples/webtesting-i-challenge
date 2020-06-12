5c1b4bc29ff9ff3ff950257d8df88b9e0fa08b66
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-06-12 11:27:35 -0500

    MVP MET

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,33 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
-};
-
-function succeed(item) {
-  return { ...item };
+	succeed,
+	fail,
+	repair,
+	get,
 }
 
-function fail(item) {
-  return { ...item };
+function succeed(item) {
+	if (item.enhancement < 20) {
+		return { ...item, enhancement: item.enhancement + 1 }
+	} else {
+		return { ...item }
+	}
+}
+
+function fail(item) {if (item.enhancement < 15){
+  return { ...item, durability: -5 }
+}
+  else if (item.enhancement > 15) {
+    return {...item, durability: -10}
+  }
+  else if (item.enhancement > 16) {
+    return{...item, enhancement: -1}
+  }
 }
 
 function repair(item) {
-  return { ...item };
+	return { ...item, durability: 100 }
 }
 
 function get(item) {
-  return { ...item };
+  
 }
